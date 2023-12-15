b343a8de07ce1bb81a0b1335bf9159fceb397eca
kyung <bamtori1029@gmail.com>
2023-12-15 19:27:52 +0900

    ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   // notFound();
   return (
     <>
-      <h1>홈페이지!!!</h1>
+      <h1>홈페이지!!! 버전2</h1>
       <Counter></Counter>
       <Image
         src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
